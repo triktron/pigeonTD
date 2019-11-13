ee31a3696f2f480d4864f45e415eff532d58be2e
Triktron <ele.trik@hotmail.com>
2019-11-13 23:20:19 +0100

    added the source tiled files😄

---
+++ maps/main.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="main" tilewidth="16" tileheight="16" tilecount="308" columns="22">
+ <image source="img/snow-expansion.png" trans="000000" width="352" height="224"/>
+ <terraintypes>
+  <terrain name="grass" tile="45"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="1" terrain="0,0,0,"/>
+ <tile id="2" terrain="0,0,,0"/>
+ <tile id="3" terrain="0,,0,0"/>
+ <tile id="4" terrain=",0,0,0"/>
+ <tile id="44" terrain="0,,0,"/>
+ <tile id="45" terrain="0,0,,"/>
+ <tile id="46" terrain=",0,,0"/>
+ <tile id="47" terrain=",,0,0"/>
+ <tile id="90" terrain="0,,,"/>
+ <tile id="91" terrain=",0,,"/>
+ <tile id="92" terrain=",,,0"/>
+ <tile id="93" terrain=",,0,"/>
+</tileset>
