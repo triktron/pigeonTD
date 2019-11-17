3b07509f8df211df76e9190d31e9bd1f563e0e4c
Triktron <ele.trik@hotmail.com>
2019-11-17 13:11:50 +0100

    next version number🎉.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('mysite-dynamic').then(function(cache) {
+    caches.open('PigeonTD').then(function(cache) {
       return fetch(event.request).then(function(response) {
         cache.put(event.request, response.clone());
         return response;
